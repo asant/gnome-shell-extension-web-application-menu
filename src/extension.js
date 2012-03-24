7f522617f6adff6872c5ec98b89da63c8d73ba27
asant <andreasantilli gmx com>
2012-03-24 20:54:06 +0100

    Default installation prefix shall be the same as GS's one. Hardcode the system locale dir in metadata.json (via autotools) In "legacy mode", fetch the system locale dir from metadata.json

--- src/extension.js
+++ src/extension.js
@@ -524,7 +524,6 @@ WebAppExtension.prototype = {
 let webapps;
 let md;
 
-/* emulating python's gettext.find() behavior */
 function init_localizations(metadata) {
     let domain = metadata.uuid;
     let langs = GLib.get_language_names();
@@ -532,16 +531,13 @@ function init_localizations(metadata) {
             LOCALE_SUBDIR]));
 
     /* I prefer to fetch the uuid from the metadata instead of hardcoding it */
-    Gettext = imports.gettext.domain(metadata.uuid);
-    _ = Gettext.gettext;
+    _ = imports.gettext.domain(domain).gettext;
 
     /* check whether we're using the right shell version before trying to fetch 
      * its locale directory */
     if (imports.misc.config.PACKAGE_VERSION < NEW_API_VERSION) {
-        let data_dirs = GLib.get_system_data_dirs();
-        for (let i = 0; i < data_dirs.length; i++) {
-            locale_dirs = locale_dirs.concat([ GLib.build_filenamev([
-                    data_dirs[i], LOCALE_SUBDIR ]) ]);
+        if (metadata['system-locale-dir'] != undefined) {
+            locale_dirs = locale_dirs.concat([ metadata['system-locale-dir'] ]);
         }
     } else {
         locale_dirs = locale_dirs.concat([ imports.misc.config.LOCALEDIR ]);
