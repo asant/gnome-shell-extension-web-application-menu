5a5c9268d4297daf14de28e37be4c1f478f56bd8
asant <andreasantilli gmx com>
2012-03-24 23:22:57 +0100

    add v3.3.92 as compatible and release as v12

--- include.mk
+++ include.mk
@@ -1,9 +1,9 @@
 extension_url = https://github.com/asant/gnome-shell-extension-web-application-menu
-extension_version = 11
+extension_version = 12
 extension_name = web-application-menu
 extension_base = @atomant
 uuid = $(extension_name)$(extension_base)
-compatible_shell_versions = \"3.2\"\, \"3.2.3\"\, \"3.3.5\"\, \"3.3.90\", \"3.3.91\"
+compatible_shell_versions = \"3.2\"\, \"3.2.3\"\, \"3.3.5\"\, \"3.3.90\", \"3.3.91\"\, \"3.3.92\"
 
 topextensiondir = $(datadir)/gnome-shell/extensions
 extensiondir = $(topextensiondir)/$(uuid)
