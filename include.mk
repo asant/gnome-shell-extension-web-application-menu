cf89b280720ab9bb70e6953488020fa734bf6324
asant <andreasantilli gmx com>
2012-03-28 23:46:15 +0200

    enabled on GS 3.4.0

--- include.mk
+++ include.mk
@@ -3,7 +3,7 @@ extension_version = 13
 extension_name = web-application-menu
 extension_base = @atomant
 uuid = $(extension_name)$(extension_base)
-compatible_shell_versions = \"3.2\"\, \"3.2.3\"\, \"3.3.5\"\, \"3.3.90\", \"3.3.91\"\, \"3.3.92\"
+compatible_shell_versions = \"3.2\"\, \"3.2.3\"\, \"3.3.5\"\, \"3.3.90\", \"3.3.91\"\, \"3.3.92\"\, \"3.4\"\, \"3.4.0\"
 
 topextensiondir = $(datadir)/gnome-shell/extensions
 extensiondir = $(topextensiondir)/$(uuid)
